1637fc18b562c5d994b17e5fc4444d76e5e6ca55
leebz45 <43051856+leebz45@users.noreply.github.com>
2021-08-05 16:44:32 -0400

    Switched colors to icons
    Front of cards now have Font Awesome icons instead of colors. Window title update.

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-let colors = ["red", "orange", "yellow", "green", "blue", "violet", "indigo", "lightgreen"];
-const dupColors = [...colors];
-colors = colors.concat(dupColors);
+
+// Support for up to 24 cards (12 matches)
+let icons = ["fa-dragon", "fa-otter", "fa-kiwi-bird", "fa-dove", "fa-crow", "fa-horse", "fa-frog", "fa-fish", "fa-cat", "fa-spider",
+    "fa-dog", "fa-hippo"];
+let numMatches = 8;
+// Slice the icons array to the number of matches we have
+icons = icons.slice(0,numMatches);
+// Duplicate the array and combine it with the original so it will match the number of cards
+const dupIcons = [...icons];
+icons = icons.concat(dupIcons);
 var firstFlip = null;
 
 function shuffle(arr) {
-    // Fisher–Yates shuffle Algorithm
 
     // Shuffle 50 times
     for (let i = 0; i < 50; i++) {
+        // Fisher–Yates shuffle Algorithm
         // Start from the last element and swap
         // one by one. We don't need to run for
         // the first element that's why i > 0
@@ -17,7 +24,6 @@ function shuffle(arr) {
             // Swap arr[i] with the element
             // at random index
             [arr[i], arr[j]] = [arr[j], arr[i]];
-
         }
     }
     return arr;
@@ -25,7 +31,7 @@ function shuffle(arr) {
 
 function addCards() {
     let count = 0;
-    const rowColors = shuffle(colors);
+    const randomIcons = shuffle(icons);
     for (let i = 0; i < 2; i++) {
         let container = document.createElement("div");
         container.classList.add("container");
@@ -39,19 +45,22 @@ function addCards() {
             const card = document.createElement("div");
             card.classList.add("card");
             card.id = count;
-            card.addEventListener("click",turnCard);
+            card.addEventListener("click", turnCard);
 
             // Front of card
             const front = document.createElement("div");
             front.classList.add("front");
-            front.classList.add(rowColors[count]);
+            // front.classList.add(rowIcons[count]);
+            let icon = document.createElement("i");
+            icon.classList.add("fas", randomIcons[count], "fa-4x", "fa-fw", "cardIcon")
+            front.appendChild(icon);
 
             // Back of card
             const back = document.createElement("div");
             back.classList.add("back");
-            const ic = document.createElement("i");
-            ic.classList.add("fas", "fa-brain", "fa-4x", "fa-fw", "cardIcon");
-            back.appendChild(ic);
+            icon = document.createElement("i");
+            icon.classList.add("fas", "fa-brain", "fa-4x", "fa-fw", "cardIcon");
+            back.appendChild(icon);
 
             card.appendChild(front);
             card.appendChild(back);
@@ -64,7 +73,7 @@ function addCards() {
 }
 
 function turnCard() {
-  
+
     if (this.classList.contains("flipped")) {
         return;
     } else {
@@ -74,11 +83,11 @@ function turnCard() {
     if (!firstFlip) {
         firstFlip = this;
         return;
-    } else if (firstFlip.children[0].classList[1] !== this.children[0].classList[1]) {
-        setTimeout(function (first,second) {
+    } else if (firstFlip.children[0].children[0].classList[1] !== this.children[0].children[0].classList[1]) {
+        setTimeout(function (first, second) {
             first.classList.toggle("flipped");
             second.classList.toggle("flipped");
-        }, 1 * 1000, firstFlip,this);
+        }, 1 * 1000, firstFlip, this);
     }
     firstFlip = null;
 }
